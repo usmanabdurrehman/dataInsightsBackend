65bcbf4240aaf144f9877332a2310e00cb5413bb
Usman Abdur Rehman <saidskit@outlook.com>
2024-01-01 13:53:43 +0500

    Added BASE_PATH env var

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS,cross_origin
+from flask_cors import CORS
 import pandas as pd
 import math
 import pickle
 import json
+import os
 
 from constants import COLUMN_NAN_THRESHOLD, SAVED_MODEL_NAME
 from utils import find, get_decomposed_plot_data, get_problem_type, get_feature_importances, get_best_fit_model
 
 app = Flask(__name__)
-CORS(app,support_credentials=True) 
+CORS(app,support_credentials=True)
+
+BASE_PATH = os.getenv("BASE_PATH") or ''
 
 best_model = ''
 
 @app.route('/')
-@cross_origin(origin='*',supports_credentials=True)
 def index():
-	return 'Nice'
+	return 'This is the DataInsights.io server'
 
 @app.route('/getData', methods = ['POST'])
-@cross_origin(origin='*',supports_credentials=True)
 def getData():
 	showTicks = json.loads(request.form['showTicks'])
 	df = ''
@@ -33,16 +34,15 @@ def getData():
 		target = request.form['target']
 	elif(showTicks['heart']):
 		filename = 'heart dataset'
-		# df = pd.read_csv(r'/home/usmanabdurrehman/mysite/Data-Insights.io/heart.csv')
-		df = pd.read_csv('example_datasets/heart.csv')
+		df = pd.read_csv(BASE_PATH + 'example_datasets/heart.csv')
 		target = 'target'
 	elif(showTicks['prostate']):
 		filename = 'prostate dataset'
-		df = pd.read_csv('example_datasets/prostate.csv')
+		df = pd.read_csv(BASE_PATH + 'example_datasets/prostate.csv')
 		target = 'lpsa'
 	else:
 		filename = 'Stock Prices dataset'
-		df = pd.read_csv('example_datasets/stock_prices.csv') 
+		df = pd.read_csv(BASE_PATH +'example_datasets/stock_prices.csv')
 		target = 'Stock price'
 
 	X,y = df.drop(target,axis=1),df[target]
@@ -60,7 +60,7 @@ def getData():
 	while(j<len(null_vals)):
 		if(null_vals[j]>COLUMN_NAN_THRESHOLD):
 			columns_to_be_dropped.append(null_vals.index[j])
-		j+=1	
+		j+=1
 
 	X = X.drop(columns_to_be_dropped,axis=1)
 
@@ -84,11 +84,11 @@ def getData():
 			columns_to_be_dummied.append(i)
 		elif(X[i].dtype.name=='object' and columns_iscategorical[j]==False):
 			columns_to_be_dropped.append(i)
-		j+=1	
+		j+=1
 
 	X = pd.get_dummies(X,columns=columns_to_be_dummied)
 	X = X.drop(columns_to_be_dropped,axis=1)
-		
+
 	stats = {
 		'problemType':get_problem_type(y),
 		'numSamples':len(X),
@@ -99,7 +99,7 @@ def getData():
 
 	####       Feature Importances        ####
 	feature_importances = get_feature_importances(X,y,number_of_features_to_select=math.ceil(len(X.columns)/2))
-	feature_importances_client = []	
+	feature_importances_client = []
 	idx = 0
 
 	for feature in X.columns:
@@ -116,14 +116,14 @@ def getData():
 	plot_data['x'] = [ round(elem,2) for elem in plot_data['x']]
 	plot_data['y'] = [ round(elem,2) for elem in plot_data['y']]
 	p = 0
-	xy = []		
+	xy = []
 	for i in plot_data['x']:
 		xy.append({
 			'x':round(plot_data['x'][p],2),
 			'y':round(plot_data['y'][p],2)
 		})
 		p+=1
-	plot_data['xy'] = xy	
+	plot_data['xy'] = xy
 
 	###			Fitting a model 		###
 	model__ = get_best_fit_model(X,y)
@@ -144,7 +144,6 @@ def getData():
 	return jsonify(finalDict)
 
 @app.route('/predict', methods = ['POST'])
-@cross_origin(origin='*',supports_credentials=True)
 def predict():
 	features = request.get_json()['features']
 	filename = SAVED_MODEL_NAME
@@ -156,6 +155,5 @@ def predict():
 		'prediction':str(prediction[0])
 	})
 
-
 if __name__ == "__main__":
-	app.run(debug=True)	
+	app.run(debug=True)
